16768e54d791cc5d05587a507f11c8b1f6476d0b
Chao Fan <fanchao05@gmail.com>
2018-03-23 23:31:24 -0400

    updated SearchBar component

--- weather_app/src/containers/search_bar.jsx
+++ weather_app/src/containers/search_bar.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChnage = this.onInputChnage.bind(this);
+  }
+
+  onInputChnage(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
-      <form className="inpjt-group">
-        <input />
+      <form className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChnage} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
