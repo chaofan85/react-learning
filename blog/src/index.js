4c6ca35a7a6d715470fe87ff5f248557c922e505
Chao Fan <fanchao05@gmail.com>
2018-03-26 16:35:01 -0400

    cleaned code

--- blog/src/index.js
+++ blog/src/index.js
@@ -7,13 +7,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/app';
 import reducers from './reducers';
 
-class Hello extends React.Component {
-  render() { return <div>Hello!</div>; }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div>; }
-}
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -21,8 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        
       </div>
     </BrowserRouter>
   </Provider>
