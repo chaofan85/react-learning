4158fa2b66fdff99122d1c5c2cf74bf19986d1ee
Chao Fan <fanchao05@gmail.com>
2018-03-26 19:20:40 -0400

    added form valicating function

--- blog/src/components/posts_new.jsx
+++ blog/src/components/posts_new.jsx
@@ -19,25 +19,42 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
-          label="Title"
+          label="Title For Post"
           name="title"
           component={this.renderField}
         />
-      <Field
-          label="Tags"
-          name="tags"
+        <Field
+          label="Categories"
+          name="categories"
           component={this.renderField}
-      />
-      <Field
+        />
+        <Field
           label="Post Content"
           name="content"
           component={this.renderField}
-      />
+        />
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
