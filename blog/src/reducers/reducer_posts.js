d65ff99f0a5426b296583720fa4319b71d5b12c9
Chao Fan <fanchao05@gmail.com>
2018-03-27 17:08:30 -0400

    added deletePost reducer

--- blog/src/reducers/reducer_posts.js
+++ blog/src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
@@ -13,6 +13,8 @@ export default function(state = {}, action) {
       // return newState;
 
       return { ...state, [action.payload.data.id]: action.payload.data }
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
